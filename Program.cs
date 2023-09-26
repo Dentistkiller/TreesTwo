a9ac479766d33cf4b4b9f4917eb96c016f8960be
Dentistkiller <yusufparuk895@gmail.com>
2023-09-26 17:57:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TreesTwo
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create a sample binary tree
+            TreeNode root = new TreeNode(1);
+            root.Left = new TreeNode(2);
+            root.Right = new TreeNode(3);
+            root.Left.Left = new TreeNode(4);
+            root.Left.Right = new TreeNode(5);
+            root.Right.Left = new TreeNode(6);
+            root.Right.Right = new TreeNode(7);
+
+            // Print the tree structure
+            Console.WriteLine("Binary Tree Structure:");
+            TreePrinter.PrintTree(root);
+        }
+    }
+}
---
+++ TreePrinter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreesTwo
+{
+    class TreePrinter
+    {
+        public static void PrintTree(TreeNode root, string indent = "", bool last = true)
+        {
+            if (root == null)
+                return;
+
+            Console.Write(indent);
+            if (last)
+            {
+                Console.Write("└─");
+                indent += "  ";
+            }
+            else
+            {
+                Console.Write("├─");
+                indent += "│ ";
+            }
+
+            Console.WriteLine(root.Value);
+
+            PrintTree(root.Left, indent, false);
+            PrintTree(root.Right, indent, true);
+        }
+    }
+}
